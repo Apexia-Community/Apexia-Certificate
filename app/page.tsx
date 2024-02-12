40d35e1780c2946edb9d17a37dea8a891a95053d
imsp18 <shivangparmar537@gmail.com>
2024-02-12 23:58:51 +0530

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Hero from "@/components/universal/Hero";
+
 export default function Home() {
     return (
         <>
+            <Hero />
         </>
     );
 }
---
+++ components/universal/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold">
+                Welcome to Apexia Certificate
+            </h1>
+        </div>
+    )
+}

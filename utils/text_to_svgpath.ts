9bd4726431acfe89b3fabb09d19cb8260cd49b85
Code-Parth <softsparths@gmail.com>
2024-02-16 15:07:04 +0530

    Update font and position in text_to_svgpath.ts

--- utils/text_to_svgpath.ts
+++ utils/text_to_svgpath.ts
@@ -1,9 +1,9 @@
 const TextToSVG = require('text-to-svg');
 
 const attributes = { fill: 'red', stroke: 'black' };
-const options = { x: 0, y: 0, fontSize: 24, anchor: 'top', attributes: attributes };
+const options = { x: 40, y: 306, fontSize: 24, attributes: attributes };
 
 // Argument is file path (NOT URL)
-const textToSVG = TextToSVG.loadSync('./public/fonts/borna.ttf');
-const svg = textToSVG.getSVG('SHIVANG PARMAR', options);
+const textToSVG = TextToSVG.loadSync('./public/fonts/MARTIANMONO_SEMIEXPANDED-MEDIUM.otf');
+const svg = textToSVG.getD('SHIVANG PARMAR', options);
 console.log(svg);

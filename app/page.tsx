daa514cb4ad822d122edd4eb24569496d038873d
imsp18 <shivangparmar537@gmail.com>
2024-02-16 10:44:23 +0530

    Fixed Hero, Footer, Header

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Footer from "@/components/universal/Footer";
 export default function Home() {
     return (
         <>
-            <Header />
             <Hero />
         </>
     );
--- components/universal/Footer.tsx
+++ components/universal/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const year = new Date().getFullYear();
 
     return (
-        <footer className="flex justify-center items-center w-full h-20 border-t border text-black  dark:text-white mt-10">
+        <footer className="flex justify-center items-center w-full h-20 border-t text-black  dark:text-white mt-10">
             <p className="text-center">©{year} Apexia</p>
         </footer>
     );

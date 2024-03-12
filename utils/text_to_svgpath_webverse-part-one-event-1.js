c0bf57bbe75c91d2630b75db644b9e21e96eba78
Code-Parth <softsparths@gmail.com>
2024-03-12 12:55:54 +0530

    Update font and SVG attributes in text_to_svgpath_webverse-part-one-event-1.js

--- utils/text_to_svgpath_webverse-part-one-event-1.js
+++ utils/text_to_svgpath_webverse-part-one-event-1.js
@@ -2,7 +2,7 @@ const TextToSVG = require('text-to-svg');
 const path = require('path');
 
 export default async function generateSVGText(text) {
-    const fontPath = path.resolve(__dirname, '/fonts/READYFORANYTHINGBB-REGULAR.TTF');
+    const fontPath = path.resolve(__dirname, '/fonts/READYFORANYTHINGBB-BOLD.TTF');
 
     return new Promise((resolve, reject) => {
         TextToSVG.load(fontPath, function (err, textToSVG) {
@@ -11,7 +11,7 @@ export default async function generateSVGText(text) {
                 return;
             }
 
-            const svg = textToSVG.getD(text, { x: 24, y: 305, fontSize: 24 });
+            const svg = textToSVG.getD(text, { x: 24, y: 305, fontSize: 24, attributes: { fill: 'red', stroke: 'black' } });
             resolve(svg);
         });
     });
